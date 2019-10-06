621a67c18707edf15a40ad87e02e0857e59b44c0
anwerrumi <55588926+anwerrumi@users.noreply.github.com>
2019-10-06 04:19:41 -0600

    web design

---
+++ new design/let.js
@@ -0,0 +1,14 @@
+function boo(){
+    for(var i= 0; i<4; i++ ){
+        console.log('learn hunter');
+    }
+  console.log(i);
+}
+
+boo();
+
+
+function name(n){
+    return n;
+}
+console.log(name('Hello world'));
---
+++ new design/variable.js
@@ -0,0 +1,20 @@
+
+let a= 10;
+let b=a;
+let c= b;
+a+=10;
+b+=20;
+c+=30;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let i=0;
+while(i<3){
+    console.log(i);
+    i++;
+}
+
+for( let x=0;x<3;x++){
+    console.log(x);
+}
